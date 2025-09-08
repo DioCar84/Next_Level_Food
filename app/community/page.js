a7ff672bad96171a244b01da0ba4bd2fd1bcde93
Diogo Carvalho <diogo.carvalho@nanoporetech.com>
2025-09-08 10:28:24 +0100

    Add project skeleton and some styles

---
+++ app/community/page.js
@@ -0,0 +1,9 @@
+export default function Community() {
+  return (
+    <main>
+      <h1 style={{ color: "white", textAlign: "center" }}>
+        Welcome to the Community Page!
+      </h1>
+    </main>
+  );
+}
---
+++ app/meals/[mealSlug]/page.js
@@ -0,0 +1,9 @@
+export default function MealDetail({ params }) {
+  return (
+    <main>
+      <h1 style={{ color: "white", textAlign: "center" }}>
+        Welcome to the Slug Page!
+      </h1>
+    </main>
+  );
+}
